74a7d363d1d4368b520952eb7a7fb9154deebe3e
[ricky pels] <[rickypels3@gmail.com]>
2024-04-17 16:56:28 +0700

    Hapus bagian yang redundan terhadap nav yang lama

--- src/Xdom/misc/ProtectedRoute.js
+++ src/Xdom/misc/ProtectedRoute.js
@@ -3,95 +3,11 @@ import { Outlet, Navigate, Link, NavLink, useLocation, useNavigate } from "react
 import { AuthContextFM } from "./contextApp";
 import { Button } from "react-bootstrap";
 import { auth } from "../../config/firebase";
-import Logo from "../Resources/logo.png";
 import UserImage from "../Resources/defaultuser.png";
 import "../misc/style.css";
 import useAuth from "./useAuth";
 import Cookies from "js-cookie";
-
-const Navbar = () => {
-  const { userInfo } = useContext(AuthContextFM);
-  React.useEffect(() => {
-    console.log("Role : ", userInfo.role);
-  }, []);
-
-  return (
-    <nav>
-      <div class="sidenav">
-        <div class="row">
-          <div className="logo">
-            <img style={{ height: 100 }} src={Logo} alt="" />
-          </div>
-          <div>
-            <div
-              class="nav flex-column nav-pills"
-              id="v-pills-tab"
-              role="tablist"
-              aria-orientation="vertical"
-            >
-              <div className="tab">
-                <NavLink
-                  exact
-                  to="/transaction"
-                  className="nav-link"
-                  activeClassName="active"
-                  role="tab"
-                  aria-controls="v-pills-home"
-                  aria-selected="true"
-                >
-                  Transaction
-                </NavLink>
-              </div>
-              <div className="tab">
-                <NavLink
-                  exact
-                  to="/players"
-                  className="nav-link"
-                  activeClassName="active"
-                  role="tab"
-                  aria-controls="v-pills-home"
-                  aria-selected="true"
-                >
-                  Players
-                </NavLink>
-              </div>
-              <div className="tab">
-                <NavLink
-                  exact
-                  to="/banks"
-                  className="nav-link"
-                  activeClassName="active"
-                  role="tab"
-                  aria-controls="v-pills-home"
-                  aria-selected="true"
-                >
-                  Banks
-                </NavLink>
-              </div>
-              {userInfo.username == "admin" ? (
-                <div className="tab">
-                  <NavLink
-                    exact
-                    to="/accountcenter"
-                    className="nav-link"
-                    activeClassName="active"
-                    role="tab"
-                    aria-controls="v-pills-home"
-                    aria-selected="true"
-                  >
-                    Account Management
-                  </NavLink>
-                </div>
-              ) : (
-                <div></div>
-              )}
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-};
+import Dashboard from '../mainpage/index'
 
 const PrivateRoutes = () => {
   const navigate = useNavigate()
@@ -111,59 +27,7 @@ const PrivateRoutes = () => {
   };
   return loginstatus ? (
     <div id="app" className="row">
-      <div class="col-md-3">
-        <Navbar />
-      </div>
-      <div style={{ backgroundColor: "#427D9D" }} class="col-md-9">
-        <div
-          className="row"
-          style={{ padding: "20px", backgroundColor: "white" }}
-        >
-          <div className="col-md-6">
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <div style={{ margin: "5px" }}>
-                <img
-                  style={{
-                    border: "1px solid black",
-                    borderRadius: 50,
-                    height: 100,
-                  }}
-                  src={UserImage}
-                  alt="user image"
-                />
-              </div>
-              <div style={{ margin: "5px" }}>
-                <h3>Anda login sebagai :</h3>
-                <h3>{userInfo.user_entry_name}</h3>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6" style={{ textAlign: "right" }}>
-            <Button onClick={logOutHandling}>Logout</Button>
-          </div>
-        </div>
-        <div style={{ margin: "5px", color: "white" }}>
-          {pathname == "/transaction"
-            ? "Transaction"
-            : pathname == "/players"
-            ? "Player"
-            : pathname == "/banks"
-            ? "Banks"
-            : pathname == "/accountcenter"
-            ? "Account Management"
-            : ""}
-        </div>
-        <div
-          style={{
-            backgroundColor: "white",
-            boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.3)",
-            borderRadius: 5,
-            margin: "20px",
-          }}
-        >
-          <Outlet />
-        </div>
-      </div>
+      <Dashboard/>
     </div>
   ) : (
     <Navigate to="/login" />
